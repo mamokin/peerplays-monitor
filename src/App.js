34257f6da539c3055d4a497118f10d5db151a067
Michael Seaward <mamoplays@gmail.com>
2019-05-16 20:06:45 -0300

    chore: initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {ConnectedRouter} from 'connected-react-router/immutable';
+import routes from './routes';
+import {create} from 'jss';
+import JssProvider from 'react-jss/lib/JssProvider';
+import {createGenerateClassName, jssPreset} from '@material-ui/core/styles';
+
+const styleNode = document.createComment('insertion-point-jss');
+document.head.insertBefore(styleNode, document.head.firstChild);
+
+const generateClassName = createGenerateClassName();
+const jss = create({
+  ...jssPreset(),
+  insertionPoint: 'insertion-point-jss'
+});
+
+const App = ({history}) => {
+  return (
+    <JssProvider jss={ jss } generateClassName={ generateClassName }>
+      <ConnectedRouter history={ history }>
+        {routes}
+      </ConnectedRouter>
+    </JssProvider>
+  );
+};
+
+App.propTypes = {
+  history: PropTypes.object
+};
+
+export default App;
